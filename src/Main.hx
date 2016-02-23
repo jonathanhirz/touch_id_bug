1d1171971f0e0574bbdf546acf1853164c1da666
Jonathan Hirz <jonathanhirz@gmail.com>
2016-02-23 11:25:41 -0800

    initial commit

---
+++ src/Main.hx
@@ -0,0 +1,33 @@
+
+import luxe.Input;
+
+class Main extends luxe.Game {
+
+    override function config(config:luxe.AppConfig) {
+
+        return config;
+
+    } //config
+
+    override function ready() {
+
+    } //ready
+
+    override function onkeyup( e:KeyEvent ) {
+
+        if(e.keycode == Key.escape) {
+            Luxe.shutdown();
+        }
+
+    } //onkeyup
+
+    override function update(dt:Float) {
+
+    } //update
+
+    override function ontouchdown(e:TouchEvent) {
+        trace(e.touch_id);
+    }
+
+
+} //Main
